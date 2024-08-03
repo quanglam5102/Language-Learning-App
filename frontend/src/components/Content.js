7a7ac79d3ecd0b3ee70a0a99fc67b5b6cd7cb88d
ShivV21 <93798896+ShivV21@users.noreply.github.com>
2024-08-03 17:33:56 -0400

    Update Content.js

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -1,13 +1,33 @@
-import { dividerClasses } from '@mui/material';
-import React from 'react';
+.App {
+  text-align: center;
+}
 
-const Content = () => {
+.App-logo {
+  animation: App-logo-spin infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+}
 
-    return (
-        <div>
-            <h1>This is the content page.</h1>
-        </div>
-    );
-};
+.App-header {
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
 
-export default Content;
+.App-link {
+  color: #61dafb;
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
