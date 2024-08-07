d794139aca9287475b195e5fe2fcc325fc421350
quanglam5102 <quanglam5102@gmail.com>
2024-08-07 12:23:34 -0400

    fixed some issue in urls.py

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,12 +5,7 @@ urlpatterns = [
     path('', index),
     path('login', index),
     path('register', index),
-<<<<<<< HEAD
     path('profile', index),
     path('content', index),
     path('about', index),
 ]
-=======
-    path('content', index)
-]
->>>>>>> 78a02c8e7b4f7cfe8879bb3e8d497662a5f19df8
