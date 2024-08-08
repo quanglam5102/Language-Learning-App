87a49494a0dc468773e8de7474f99de13cf08718
quanglam5102 <quanglam5102@gmail.com>
2024-08-08 08:44:05 -0400

    added user model

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main
+from .views import UserView, CreateUserView
 
 urlpatterns = [
-    path('home', main)
+    path('user', UserView.as_view()),
+    path('create-user', CreateUserView.as_view())
 ]
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'
 
 import '../../static/css/home.css';
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Language Learner!</h1>
         <p>Start your journey to becoming fluent in multiple languages today.</p>
-        <button className="App-button" onClick={() => alert('Get Started Clicked!')}>
+        <button className="App-button" onClick={() => navigate('/content')}>
           Get Started
         </button>
       </header>
