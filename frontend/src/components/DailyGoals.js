76b9be2c44224c4c72542171575040f8107b343f
ShivV21 <93798896+ShivV21@users.noreply.github.com>
2024-08-14 12:57:24 -0400

    Create DailyGoals.js

---
+++ frontend/src/components/DailyGoals.js
@@ -0,0 +1,41 @@
+const addGoal = () => {
+    if (inputValue.trim() !== '') {
+        setGoals([...goals, { text: inputValue, completed: false }]);
+        setInputValue('');
+    }
+};
+
+return (
+    <div className="container">
+        <h1>Daily Goals</h1>
+        <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Enter your goal"
+        />
+        <button onClick={addGoal}>Add Goal</button>
+        <ul>
+            {goals.map((goal, index) => (
+                <li key={index}>
+                    <input
+                        type="checkbox"
+                        checked={goal.completed}
+                        onChange={() => {
+                            const newGoals = [...goals];
+                            if (newGoals[index].completed) {
+                                newGoals.splice(index, 1);
+                            } else {
+                                newGoals[index].completed = !newGoals[index].completed;
+                            }
+                            setGoals(newGoals);
+                        }}
+                    />
+                    <span style={{ textDecoration: goal.completed ? 'line-through' : 'none' }}>
+                        {goal.text}
+                    </span>
+                </li>
+            ))}
+        </ul>
+    </div>
+);
